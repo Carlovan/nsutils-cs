04297c8f46696003b6dea1c079bb9aba12e109af
Carlovan <giulio.carlassare@gmail.com>
2015-04-22 23:46:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nsutils-cs
+Library to add some Non Standart UTILitieS to your C# program.
---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSUtils;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string[] l = { "ciao", "cacca", "cielo", "minestra", "ciaone"};
+            PrefixTree t = new PrefixTree(l);
+
+			t.Print();
+
+            foreach (string x in t.GetAllWithPrefix("ci"))
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
